58c43198897ca5de5e2b6662db6ef6647383055f
Jordan Fischer <jordan@thefischers.org>
2021-01-25 13:43:10 -0500

    Wrote a program that loops the specified number of times (between 1 and 100)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace D2_Iteratives
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Please enter a number between 1 and 100...");
+
+			if (int.TryParse(Console.ReadLine(), out int numLoops) && numLoops >= 1 && numLoops <= 100)
+				for (int i = 1; i <= numLoops; i++)
+					Console.WriteLine($"You have entered {numLoops}. This is the current integer value in the loop: {i}");
+			else
+				Console.WriteLine("You must enter an integer value between 1 and 100!");
 		}
 	}
 }
